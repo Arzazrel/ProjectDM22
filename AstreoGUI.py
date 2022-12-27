5092f1e2795d4ed38fba6747cfe058763ca4775f
Arzazrel <88975224+Arzazrel@users.noreply.github.com>
2022-12-27 21:36:41 +0100

    Create GUI file

---
+++ AstreoGUI.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+from tkinter import *
+
+# global values
+window = Tk()
+
+if __name__ == "__main__":
+	window.title("Astreo")
+	window.geometry('600x600')
+	
+	window.mainloop()
